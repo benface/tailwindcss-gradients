0f01e2f68ae8c1f325f1a805eb54ac7694945a7a
Kani Robinson <me@kanirobinson.co.uk>
2018-10-16 21:14:40 +0100

    Changed to a more conventional namespacing `bg-gradient-[direction]-[color]`

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ module.exports = ({
             [`.${e(`bg-gradient-to-bottom-right-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom right, ${colors.join(', ')})` },
             [`.${e(`bg-gradient-to-bottom-left-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom left, ${colors.join(', ')})` },
             [`.${e(`bg-gradient-to-left-${name}`)}`]: { backgroundImage: `linear-gradient(to left, ${colors.join(', ')})` },
-            [`.${e(`bg-g-t-${name}`)}`]: { backgroundImage: `linear-gradient(to top, ${colors.join(', ')})` },
-            [`.${e(`bg-g-t-r-${name}`)}`]: { backgroundImage: `linear-gradient(to top right, ${colors.join(', ')})` },
-            [`.${e(`bg-g-t-l-${name}`)}`]: { backgroundImage: `linear-gradient(to top left, ${colors.join(', ')})` },
-            [`.${e(`bg-g-r-${name}`)}`]: { backgroundImage: `linear-gradient(to right, ${colors.join(', ')})` },
-            [`.${e(`bg-g-b-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom, ${colors.join(', ')})` },
-            [`.${e(`bg-g-b-r-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom right, ${colors.join(', ')})` },
-            [`.${e(`bg-g-b-l-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom left, ${colors.join(', ')})` },
-            [`.${e(`bg-g-l-${name}`)}`]: { backgroundImage: `linear-gradient(to left, ${colors.join(', ')})` }
+            [`.${e(`bg-gradient-t-${name}`)}`]: { backgroundImage: `linear-gradient(to top, ${colors.join(', ')})` },
+            [`.${e(`bg-gradient-tr-${name}`)}`]: { backgroundImage: `linear-gradient(to top right, ${colors.join(', ')})` },
+            [`.${e(`bg-gradient-tl-${name}`)}`]: { backgroundImage: `linear-gradient(to top left, ${colors.join(', ')})` },
+            [`.${e(`bg-gradient-r-${name}`)}`]: { backgroundImage: `linear-gradient(to right, ${colors.join(', ')})` },
+            [`.${e(`bg-gradient-b-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom, ${colors.join(', ')})` },
+            [`.${e(`bg-gradient-br-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom right, ${colors.join(', ')})` },
+            [`.${e(`bg-gradient-bl-${name}`)}`]: { backgroundImage: `linear-gradient(to bottom left, ${colors.join(', ')})` },
+            [`.${e(`bg-gradient-l-${name}`)}`]: { backgroundImage: `linear-gradient(to left, ${colors.join(', ')})` }
           };
         }),
       ),
