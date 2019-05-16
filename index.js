c3d715854a161d658eb5bdc9b93d7d3a9aa5da9d
Benoît Rouleau <benoit.rouleau@icloud.com>
2019-05-16 15:13:50 -0400

    Code cleanup

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const _ = require('lodash');
 const Color = require('color');
 
 module.exports = function() {
-  return ({ config, e, addUtilities, variants }) => {
-    const defaultDirections = {
+  return ({ theme, variants, e, addUtilities }) => {
+    const defaultGradientDirections = {
       't': 'to top',
       'tr': 'to top right',
       'r': 'to right',
@@ -13,18 +13,22 @@ module.exports = function() {
       'l': 'to left',
       'tl': 'to top left',
     };
-    const defaultColors = {};
-    const defaultVariants = ['responsive'];
+    const defaultGradientColors = {};
+    const defaultGradientVariants = ['responsive'];
+
+    const gradientDirections = theme('gradients.directions', defaultGradientDirections);
+    const gradientColors = theme('gradients.colors', defaultGradientColors);
+    const gradientVariants = variants('gradients', defaultGradientVariants);
 
     const gradientUtilities = (function() {
       let utilities = {};
-      _.forEach(config('theme.gradients.colors', defaultColors), (colors, colorKey) => {
+      _.forEach(gradientColors, (colors, colorKey) => {
         if (!_.isArray(colors) || colors.length === 1) {
           let color = _.isArray(colors) ? colors[0] : colors;
           let parsedColor = Color(color);
           colors = [parsedColor.alpha(0).rgb().string(), color];
         }
-        _.forEach(config('theme.gradients.directions', defaultDirections), (direction, directionKey) => {
+        _.forEach(gradientDirections, (direction, directionKey) => {
           utilities[`.${e(`bg-gradient-${directionKey}-${colorKey}`)}`] = {
             backgroundImage: `linear-gradient(${direction}, ${colors.join(', ')})`,
           };
@@ -33,6 +37,6 @@ module.exports = function() {
       return utilities;
     })();
 
-    addUtilities(gradientUtilities, variants('gradients', defaultVariants));
+    addUtilities(gradientUtilities, gradientVariants);
   };
 };
